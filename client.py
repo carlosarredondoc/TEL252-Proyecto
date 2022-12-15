62333e0db0c6bdd87f749f5e6d7c6172f33913da
jestej <vicentetejos1@gmail.com>
2022-12-15 04:50:29 -0300

    Try peer

--- client.py
+++ client.py
@@ -1,14 +1,45 @@
 import requests
 import random as r
 import time
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
-url = 'http://193.203.12.121/sensor'
-id = r.randint(0,50)
 
+url = 'http://localhost:8000/sensor'
+id = 1
+
+#######################
+# Encriptación ECDSA  #
+#######################
+
+
+
+device_private_key = ec.generate_private_key(
+    ec.SECP384R1()
+)
+
+peer_private_key = ec.generate_private_key(
+    ec.SECP384R1()
+)
+
+
+
+
+shared_key = device_private_key.exchange(
+    ec.ECDH(), peer_private_key.public_key())
+
+derived_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=128,
+    salt=None,
+    info="tel252_loselipticos".encode(),
+).derive(shared_key)
+
+##########################################
 while True:
     temp = r.uniform(5,40)
-    sensor = {"id": id, "temp":temp}
-
+    sensor = {"id": id, "temp":temp, "peer_priv_key": peer_private_key , "device_priv_key": device_private_key , "shared_key": str(derived_key)}
     print(sensor)
     requests.post(url, json = sensor)
-    time.sleep(2)
+    time.sleep(3) 
