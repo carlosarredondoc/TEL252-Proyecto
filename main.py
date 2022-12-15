38472cdd006ac97aa112fbec4d8a61ff6f3a5166
jestej <vicentetejos1@gmail.com>
2022-12-15 04:49:52 -0300

    Try with curve eliptic peer_priv_key

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
 app = FastAPI()
 
 data_sensor = {}
@@ -16,18 +20,54 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+##
+
+
+
+
+# Perform key derivation.
+
+
 class Sensor(BaseModel):
     id: str
     timestamp: None | datetime
     temp: str
+    peer_priv_key: str
+    device_priv_key: str
+    shared_key: str
+import sys
+def str_to_class(str):
+    return getattr(sys.modules[__name__], str)
+
 
 @app.post("/sensor")
 async def upload_data(sensor:Sensor):
-    sensor.timestamp = datetime.now()
-    json_compatible_item_data = jsonable_encoder(sensor)      
-    data_sensor[sensor.id] = json_compatible_item_data
-    return JSONResponse(content=json_compatible_item_data)
+    
+    peer_priv_key = str_to_class(sensor.peer_priv_key)
+    
+    device_private_key = str_to_class(sensor.device_priv_key)
+    
 
+    shared_key_dev = sensor.shared_key
+    print("a")
+
+    same_shared_key = peer_priv_key.exchange(
+    ec.ECDH(), device_private_key.public_key())
+    
+    same_derived_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=128,
+    salt=None,
+    info='tel252_loselipticos'.encode(),
+    ).derive(same_shared_key)
+
+    if same_derived_key == shared_key_dev:
+        sensor.timestamp = datetime.now()
+        json_compatible_item_data = jsonable_encoder(sensor)      
+        data_sensor[sensor.id] = json_compatible_item_data
+        return JSONResponse(content=json_compatible_item_data)
+    else:
+        return JSONResponse(status_code=403, content="Forbidden Request!")
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     print(data_sensor)
