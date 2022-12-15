5c2659dfdf2f2e4296a1ef4ca35f8f149203731c
jestej <vicentetejos1@gmail.com>
2022-12-15 11:32:11 -0300

    ECDSA funcionando...

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
 import requests
 import random as r
 import time
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from ecdsa import SigningKey, BRAINPOOLP160r1
 
 
 url = 'http://localhost:8000/sensor'
@@ -12,34 +10,17 @@ id = 1
 #######################
 # Encriptación ECDSA  #
 #######################
-
-
-
-device_private_key = ec.generate_private_key(
-    ec.SECP384R1()
-)
-
-peer_private_key = ec.generate_private_key(
-    ec.SECP384R1()
-)
-
-
-
-
-shared_key = device_private_key.exchange(
-    ec.ECDH(), peer_private_key.public_key())
-
-derived_key = HKDF(
-    algorithm=hashes.SHA256(),
-    length=128,
-    salt=None,
-    info="tel252_loselipticos".encode(),
-).derive(shared_key)
-
+sk = SigningKey.generate(curve=BRAINPOOLP160r1)
+vk = sk.verifying_key
+certificado1 = vk.to_string()
+certificado =certificado1.hex()
 ##########################################
 while True:
     temp = r.uniform(5,40)
-    sensor = {"id": id, "temp":temp, "peer_priv_key": peer_private_key , "device_priv_key": device_private_key , "shared_key": str(derived_key)}
+    sensor = {"id": id, "temp":temp, "cert": certificado }
+    print(type(certificado1),certificado1)
+    print(type(certificado),certificado)
+    #print()
     print(sensor)
     requests.post(url, json = sensor)
     time.sleep(3) 
